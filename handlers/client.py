0e48c7bcde826f19ff58e680690db9b1a7322832
LevasyukDY <Levasyuk@gmail.com>
2022-03-29 18:48:36 +0900

    добавлено взаимодействие с API через пакет requests

--- handlers/client.py
+++ handlers/client.py
@@ -1,6 +1,10 @@
+from tkinter import E
+from turtle import title
 from aiogram import Dispatcher, types
 from create_bot import dp, bot
 from keyboards import kb_client
+import requests
+from pprint import pprint
 
 
 async def command_start(message : types.Message):
@@ -11,5 +15,30 @@ async def command_start(message : types.Message):
   except:
     await message.reply('Общение с ботом происходит в лс, напишите ему!')
 
+async def get_news(message : types.Message):
+  try:
+    if message.text.lower() == 'новости':
+      r = requests.get("http://127.0.0.1:8000/api/news")
+      data = r.json()
+
+      # Метод send_photo() ищет картинки с серверов телеграм и не находит нашу локальную БД
+      # storageURL = "http://127.0.0.1:8000/storage/"
+      # photoURL = ""
+
+      i = 0
+      while i < len(data):
+        # photoURL = storageURL + data[i]["preview"]
+        await bot.send_photo(
+          chat_id=message.chat.id, 
+          photo="https://www.morristourism.org/wp-content/uploads/2017/09/this-is-a-test-image.png", 
+          caption=data[i]["title"]
+        )
+        i += 1
+  except Exception as ex:
+    print(ex)
+    print('Ошибка получения списка новостей')
+
+
 def register_handlers_client(dp : Dispatcher):
   dp.register_message_handler(command_start, commands=['start', 'help'])
+  dp.register_message_handler(get_news)
--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -2,7 +2,8 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemo
 
 b1 = KeyboardButton('Персоналии')
 b2 = KeyboardButton('Расписание')
+b3 = KeyboardButton('Новости')
 
 kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
 
-kb_client.row(b1, b2)
+kb_client.row(b1, b2, b3)
